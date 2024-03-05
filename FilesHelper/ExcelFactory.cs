9bda1f7dfc99c3199b77ca0ee3da23691fd7377c
victor montebello <montebellovictor@gmail.com>
2024-03-04 21:45:52 -0300

    creating project

---
+++ FilesHelper/ExcelFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace FilesHelper
+{
+    public class ExcelFactory
+    {
+        public static ExcelFactory Instance { get { return new ExcelFactory(); } }
+
+        public ExcelFactory() { }
+
+        public FileStream GenerateEmpty()
+        {
+            return null;
+        }
+
+        public FileStream GenerateEmpty(List<string> columns)
+        {
+            return null;
+        }
+    }
+}
