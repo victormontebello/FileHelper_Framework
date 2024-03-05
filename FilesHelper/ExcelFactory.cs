3f3fbe7834f3f46848c463b13197b51d8a5eb8fc
victor montebello <montebellovictor@gmail.com>
2024-03-05 16:45:53 -0300

    generate methods

--- FilesHelper/ExcelFactory.cs
+++ FilesHelper/ExcelFactory.cs
@@ -1,4 +1,6 @@
-﻿namespace FilesHelper
+﻿using OfficeOpenXml;
+
+namespace FilesHelper
 {
     public class ExcelFactory
     {
@@ -6,14 +8,110 @@
 
         public ExcelFactory() { }
 
-        public FileStream GenerateEmpty()
+        public static byte[] GenerateEmpty(string filename)
         {
-            return null;
+            filename = string.IsNullOrEmpty(filename) ? "default" : filename;
+            filename += ".xlsx";
+
+            var downloadsFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + $"\\Downloads\\{filename}";
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                using (var worksheet = package.Workbook.Worksheets.Add(filename))
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    package.SaveAs(memoryStream);
+                    memoryStream.Position = 0;
+                }
+
+                return package.GetAsByteArray();
+            }
         }
 
-        public FileStream GenerateEmpty(List<string> columns)
+        public static byte[] GenerateEmpty(List<string> columns, string? filename)
         {
-            return null;
+            var firstRow = 1;
+            filename = string.IsNullOrEmpty(filename) ? "default" : filename;
+            filename += ".xlsx";
+
+            var downloadsFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + $"\\Downloads\\{filename}";
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                using (var worksheet = package.Workbook.Worksheets.Add(filename))
+                {
+                    columns.AsParallel().ForAll(column =>
+                    {
+                        worksheet.Cells[firstRow, columns.IndexOf(column) + firstRow].Value = columns[columns.IndexOf(column)];
+                        worksheet.Column(columns.IndexOf(column) + firstRow).AutoFit();
+                    });
+                }
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    package.SaveAs(memoryStream);
+                    memoryStream.Position = 0;
+                }
+
+                return package.GetAsByteArray();
+            }
+        }
+
+        public static void GenerateEmptyAndWrite(List<string> columns, string? filename)
+        {
+            var firstRow = 1;
+            filename = string.IsNullOrEmpty(filename) ? "default" : filename;
+            filename += ".xlsx";
+
+            var downloadsFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + $"\\Downloads\\{filename}";
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                using (var worksheet = package.Workbook.Worksheets.Add(filename))
+                {
+                    columns.AsParallel().ForAll(column =>
+                    {
+                        worksheet.Cells[firstRow, columns.IndexOf(column) + firstRow].Value = columns[columns.IndexOf(column)];
+                        worksheet.Column(columns.IndexOf(column) + firstRow).AutoFit();
+                    });
+                }
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    package.SaveAs(memoryStream);
+                    File.WriteAllBytes(downloadsFolderPath, memoryStream.ToArray());
+                    memoryStream.Position = 0;
+                }
+            }
+        }
+
+        public static void GenerateEmptyAndWrite(string filename)
+        {
+            filename = string.IsNullOrEmpty(filename) ? "default" : filename;
+            filename += ".xlsx";
+
+            var downloadsFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + $"\\Downloads\\{filename}";
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                using (var worksheet = package.Workbook.Worksheets.Add(filename))
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    package.SaveAs(memoryStream);
+                    File.WriteAllBytes($"{downloadsFolderPath}", memoryStream.ToArray());
+                    memoryStream.Position = 0;
+                }
+            }
         }
     }
 }
